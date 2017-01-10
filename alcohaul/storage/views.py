189121b9fedd872e52f07d2798483641e7d66dd0
MarkLii <markli@bu.edu>
2017-01-10 18:49:22 -0500

    ...

--- alcohaul/storage/views.py
+++ alcohaul/storage/views.py
@@ -4,13 +4,23 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import alcohol
 from .serializers import alcoholSerializer
-
-
+from django.template import loader
+from django.shortcuts import render
 
 # List all booze or create a new one
 # alcohol/
 def index(request):
-	return HttpResponse("<h1>This is the alchohaul app homepage</h1>")
+	all_alcohol = alcohol.objects.all()
+	template = loader.get_template("storage/index.html")
+	context = {'all_alcohol': all_alcohol}
+	return render(request, "storage/index.html", context)
+
+
+
+def detail(request, UID):
+	return HttpResponse("<h2>Details for alcohol: " + str(alcohol.UID)  + "</h2>")
+
+
 
 
 class alcoholList(APIView):
@@ -23,7 +33,15 @@ class alcoholList(APIView):
 	def post(self):
 		pass
 
+"""
+class alcoholSearch(APIView):
 
+
+	def get(self, request):
+
+
+	def post(self):
+"""
 
 
 
